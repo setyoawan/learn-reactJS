c11c46ab929e2a331742493163517eb157318634
setyoawan <setyoawan8@gmail.com>
2020-05-25 06:33:26 +0700

    component and props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import About from './component/About';
 
-function App() {
-  return (
-    <div className="App">
+
+// component dan props
+
+// function Age(props) {
+//   return (
+//     <div>
+//       <p>age {props.age}</p> 
+//     </div>
+//   );
+// }
+
+// function About(props) {
+//   return(
+//   <p>hai my name is {props.name}, <Age age= {props.age}/></p>
+//   )
+// }
+
+class App extends Component{
+  render() {
+    return(
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>
-          Hello world
-        </h3>      
+        <h3>Hello world</h3>              
+        <About name="setyo" age="22">
+          <p>child props</p>
+        </About>
+        <About name="awan" age="20">
+          <button>store</button>
+        </About>
+        <About name="prakoso" age="21"/>  
       </header>
-    </div>
-  );
+    </div>  
+    );
+  }
 }
 
 export default App;
