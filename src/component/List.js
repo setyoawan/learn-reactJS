b2e218e78e17cee7fb7e6852901939545b481543
setyoawan <setyoawan8@gmail.com>
2020-05-28 08:29:55 +0700

    add API data

--- src/component/List.js
+++ src/component/List.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 function List(props) {
     return (
@@ -12,4 +12,50 @@ function List(props) {
     )
 }
 
-export default List
+
+class Api extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            items : [],
+            loaded : true
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(result => {
+            this.setState({
+                items : result,
+                loaded : false
+            })
+        })
+    }
+
+    
+    render() {
+        const { items, loaded} = this.state;
+        if(loaded) {
+            return(
+                <h3>loading...</h3>
+            )
+        }
+
+        return(
+            <div>
+                <ul>
+                    {
+                        items.map((item, index) => <li key={index}>{item.name}</li>)
+                    }
+                </ul>
+            </div>
+
+        )
+    }
+}
+
+export default Api
+
+
